3e0d2fb611a4d49f3e3c62ee08371fb309bb35a5
rockyweng75 <wengsc@yuntech.edu.tw>
2023-01-13 08:57:39 +0800

    臉部辨視

--- MyEmguCVExample/Form1.Designer.cs
+++ MyEmguCVExample/Form1.Designer.cs
@@ -36,21 +36,33 @@
             this.button3 = new System.Windows.Forms.Button();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(73, 16);
+            this.textBox1.Location = new System.Drawing.Point(26, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(450, 27);
             this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(529, 14);
+            this.button1.Location = new System.Drawing.Point(26, 53);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 29);
             this.button1.TabIndex = 1;
@@ -60,16 +72,16 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(73, 67);
+            this.pictureBox1.Location = new System.Drawing.Point(26, 116);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(661, 371);
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(629, 14);
+            this.button2.Location = new System.Drawing.Point(134, 53);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 29);
             this.button2.TabIndex = 3;
@@ -79,7 +91,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(740, 14);
+            this.button3.Location = new System.Drawing.Point(349, 53);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 29);
             this.button3.TabIndex = 4;
@@ -89,19 +101,109 @@
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(765, 67);
+            this.imageBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(427, 277);
+            this.imageBox1.Size = new System.Drawing.Size(300, 300);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 504);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(784, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "學習";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(601, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "辨視通關";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(501, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 29);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "載入";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.imageBox3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.imageBox4);
+            this.panel1.Controls.Add(this.imageBox2);
+            this.panel1.Controls.Add(this.imageBox1);
+            this.panel1.Location = new System.Drawing.Point(373, 104);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(945, 368);
+            this.panel1.TabIndex = 9;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.imageBox3.Location = new System.Drawing.Point(624, 12);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(300, 300);
+            this.imageBox3.TabIndex = 7;
+            this.imageBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "資料庫";
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.Location = new System.Drawing.Point(0, 0);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(0, 0);
+            this.imageBox4.TabIndex = 2;
+            this.imageBox4.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.imageBox2.Location = new System.Drawing.Point(318, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(300, 300);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1398, 630);
-            this.Controls.Add(this.imageBox1);
+            this.ClientSize = new System.Drawing.Size(1373, 601);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
@@ -111,6 +213,11 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,5 +232,14 @@
         private Button button3;
         private ColorDialog colorDialog1;
         private Emgu.CV.UI.ImageBox imageBox1;
+        private Label label1;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Panel panel1;
+        private Label label2;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
     }
 }
--- MyEmguCVExample/Form1.cs
+++ MyEmguCVExample/Form1.cs
@@ -4,23 +4,36 @@ using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using System;
 using System.Windows.Forms;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
 
 namespace MyEmguCVExample
 {
     public partial class Form1 : Form
     {
         string imagePath = "";
-        Image<Bgr, byte> image; 
+        Image<Bgr, byte> image;
         bool isCanny = false;
+        double faceBase = 160;
+        string sourcePath = "faces.dat";
+        string imagesFolder = "./Data";
+
+        // https://thispersondoesnotexist.com/
 
         public Form1()
         {
             InitializeComponent();
-        }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
 
+            if (!File.Exists(sourcePath))
+            {
+                File.Create(sourcePath);
+            }
+            else 
+            {
+                //File.Delete(sourcePath);
+                //File.Create(sourcePath);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -29,7 +42,7 @@ namespace MyEmguCVExample
             // 顯示預設路徑。
             ofd.InitialDirectory = "./";
             // 設定檔案儲存類型範例為.bmp若要增加其他類型可使用{名稱}|.{類型}。
-            ofd.Filter = "Bitmap Files|*.bmp";
+            ofd.Filter = "Bitmap Files|*.bmp|jpg|*.jpg";
             ofd.Title = "選擇影像檔案";
             // 紀錄最後視窗路徑位置。
             ofd.RestoreDirectory = true;
@@ -51,7 +64,7 @@ namespace MyEmguCVExample
                 // 為 Bitmap 讀取方式。
                 // Bitmap bitmap = new Bitmap(m_imagePath);
                 // 該方法則為 EmguCV 的讀取方式。
-                image = new Image<Bgr, byte>(path);
+                image = new Image<Bgr, byte>(path).Resize(300, 300, Inter.Cubic);
                 pictureBox1.Image = (Bitmap)image.ToBitmap().Clone();
                 pictureBox1.Invalidate();
             }
@@ -71,7 +84,7 @@ namespace MyEmguCVExample
                 pictureBox1.Image = (Bitmap)cannyFrame.ToBitmap().Clone();
                 isCanny = true;
             }
-            else 
+            else
             {
                 pictureBox1.Image = (Bitmap)image.ToBitmap().Clone();
                 isCanny = false;
@@ -89,10 +102,8 @@ namespace MyEmguCVExample
         /// <param name="objMat"></param>
         /// <param name="index"></param>
         /// <returns></returns>
-        private void CaptureFace()
+        private List<Rectangle> CaptureFace(UMat umat)
         {
-            var umat = image.ToUMat();
-
             List<Rectangle> faces = new List<Rectangle>();
 
             using (CascadeClassifier faceDetector = new CascadeClassifier("./haarcascade_frontalface_default.xml"))
@@ -115,20 +126,173 @@ namespace MyEmguCVExample
                     faces.AddRange(facesDetected);
                 }
             }
-
-
-            // 在影像上進行框線的繪圖
-            for (int f = 0; f < faces.Count; f++)
-                CvInvoke.Rectangle(umat, faces[f], new Bgr(Color.Red).MCvScalar, 2);
-  
-            imageBox1.Image = umat;
-
+            return faces;
         }
 
 
+        private void PrintFace(UMat umat, List<Rectangle> faces)
+        {
+            // 在影像上進行框線的繪圖
+            for (int f = 0; f < faces.Count; f++)
+                CvInvoke.Rectangle(umat, faces[f], new Bgr(Color.Red).MCvScalar, 2);
+
+            imageBox1.Image = umat;
+        }
+
+        private (double, int, bool) FaceRecognizer(List<Rectangle> faces) 
+        {
+
+            FileStream fileStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read);
+
+            //偵測是否為人臉
+            if (faces.Count > 0) 
+            {
+                using (var faceRecognizer = new EigenFaceRecognizer(0, double.PositiveInfinity))
+                {
+                    faceRecognizer.Read(sourcePath);
+
+                    foreach (var item in faces)
+                    {
+                        //影格圖片灰階化 並設定大小100
+                        var ToGrayFace = image
+                            .Convert<Gray, Byte>()
+                            .GetSubRect(item)
+                            .Resize(100, 100, Inter.Cubic);
+
+                        if (fileStream.Length != 0)
+                        {
+                            //重新宣告
+                            var predictResult = faceRecognizer.Predict(ToGrayFace);
+
+                            //當有一筆資料吻合 其他則不再辨識
+                            if (predictResult.Distance <= faceBase)
+                            {
+                                faceRecognizer.Dispose();
+                                faceRecognizer = null;
+                                return (predictResult.Distance, predictResult.Label, true);
+                            }
+                        }
+                    }
+                }
+            }
+            return (0, 0, false);
+
+        }
+
+        /// <summary>
+        /// 單次訓練
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public int FaceTraining()
+        {
+            var facesMat = new List<Mat>();
+            var labels = new List<int>();
+
+            //取得路徑下所有照片
+            int PictureCount = 0;
+            //faceRecognizer = new EigenFaceRecognizer(80, double.PositiveInfinity);
+
+            using (var faceRecognizer = new EigenFaceRecognizer(0, double.PositiveInfinity))
+            {
+
+                //照片灰階處理
+                facesMat.Add(image.Convert<Gray, Byte>().Resize(100, 100, Inter.Cubic).Mat);
+                //照片Tag
+                labels.Add(1);
+                PictureCount += 1;
+
+                //進行訓練
+                faceRecognizer.Train(facesMat.ToArray(), labels.ToArray());
+                faceRecognizer.Write(sourcePath);
+            }
+            return PictureCount;
+        }
+
         private void button3_Click(object sender, EventArgs e)
         {
-            CaptureFace();
+
+            var umat = image.ToUMat();
+            var faces = CaptureFace(umat);
+
+            PrintFace(umat, faces);
+            //var result = FaceRecognizer(faces);
+            //label1.Text = $"比對結果 => 分數:{result.Item1}, 來源編號：{result.Item2}, 結果：{result.Item3}";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FaceTraining();
+            label1.Text = $"學習完成";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var umat = image.ToUMat();
+            var faces = CaptureFace(umat);
+            var result = FaceRecognizer(faces);
+            label1.Text = $"比對結果 => 分數:{result.Item1}, 來源編號：{result.Item2}, 結果：{result.Item3}";
+
+            umat.Dispose();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            List <Mat> trainingImages = new List<Mat>();
+            List<int> labels = new List<int>();
+            List<ImageBox> imageBoxes = new List<ImageBox>() { null, imageBox1, imageBox2, imageBox3};
+            for (var i = 1; i <= 3; i++) 
+            {
+                var _image = new Image<Bgr, byte>($"{imagesFolder}/{i}.bmp")
+                    .Resize(300, 300, Inter.Cubic);
+
+                var faces = CaptureFace(_image.ToUMat());
+
+                foreach (var item in faces) 
+                {
+                    var ToGrayFace = _image
+                       .Convert<Gray, Byte>()
+                       .GetSubRect(item)
+                       .Resize(100, 100, Inter.Cubic);
+
+                    trainingImages.Add(ToGrayFace.Mat);
+                    imageBoxes[i].Image = ToGrayFace;
+                }
+
+                labels.Add(i);
+            }
+            //MCvTermCriteria termCrit = new MCvTermCriteria(16, 0.001);
+
+            using (EigenFaceRecognizer recognizer = new EigenFaceRecognizer(80, double.PositiveInfinity))
+            {
+                recognizer.Train(trainingImages.ToArray(), labels.ToArray());
+                recognizer.Write(sourcePath);
+
+            }
+
+            using (EigenFaceRecognizer recognizer = new EigenFaceRecognizer(80, double.PositiveInfinity)) 
+            {
+                recognizer.Read(sourcePath);
+
+                var testImage = new Image<Bgr, Byte>($"{imagesFolder}/2.bmp")
+                    .Resize(300, 300, Inter.Cubic);
+
+                var faces = CaptureFace(testImage.ToUMat());
+
+                foreach (var item in faces)
+                {
+                    var ToGrayFace = testImage
+                      .Convert<Gray, Byte>()
+                      .GetSubRect(item)
+                      .Resize(100, 100, Inter.Cubic);
+
+                    EigenFaceRecognizer.PredictionResult result = recognizer.Predict(ToGrayFace);
+                    label1.Text = $"分數:{result.Distance}";
+                }
+            }
         }
 
         private void imageBox1_Click(object sender, EventArgs e)
